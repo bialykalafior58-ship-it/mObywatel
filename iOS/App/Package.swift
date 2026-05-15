d4e2274f1f79df2d2c809ee0393c0f3518343675
igus <rambriepl123@gmail.com>
2026-05-16 01:45:39 +0200

    Popraw Package.swift

--- iOS/App/Package.swift
+++ iOS/App/Package.swift
@@ -1,8 +1,25 @@
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
-  name: "app",
-  products: [],
-  targets: []
+    name: "Mobywatel",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .iOSApplication(
+            name: "Mobywatel",
+            targets: ["App"],
+            bundleIdentifier: "com.twojanazwa.mobywatel",   // ← możesz zmienić
+            displayVersion: "1.0",
+            bundleVersion: "1"
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            path: "Sources"
+        )
+    ]
 )
